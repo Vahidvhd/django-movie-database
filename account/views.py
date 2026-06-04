eb5ffbedea38fe0110aefbcf189701a5729e88a3
Vahidvhd <vahidvhd91@gmail.com>
2026-06-04 13:56:15 +0100

    Add user app and configure routes

---
+++ account/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import RegisterForm, LoginForm
+
+
+
+from django.shortcuts import render
+
+def home_view(request):
+    return render(request, 'account/home.html')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    actors = models.TextField()
+    rating = models.DecimalField(max_digits=3, decimal_places=1, validators=[MinValueValidator(0), MaxValueValidator(10)])
+    director = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+    producer = models.CharField(max_length=100, blank=True, null=True)
+    year = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
+
+
+    def __str__(self):
+        return self.title
+
