6387ed13ef6bacb5fba90376cf24cfb3d8c031e0
Vahidvhd <vahidvhd91@gmail.com>
2026-06-05 13:52:03 +0100

    Add movie search functionality

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Movie
 
 # Register your models here.
+# admin.site.register(Movie)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display=['id', 'title', 'genre', 'actors', 'rating', 'director', 'country', 'producer', 'year']
+    search_fields =['title', 'genre']
+    list_filter= ['genre']
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Movie
 
 
 def home(request):
-    return render(request, "movies/home.html")
+
+
+
+    query = request.GET.get('user_search')
+    result = Movie.objects.all()
+    if query:
+        result = Movie.objects.filter (
+            Q(title__icontains=query) |
+            Q(actors__icontains=query) )
+        
+    context = {'result': result}
+        
+    return render(request, "movies/home.html", context)
